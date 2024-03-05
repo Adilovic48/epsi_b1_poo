0903e9813ee2f988b0b48dff197a2fe5adb01811
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-03-05 15:59:09 +0100

    update

--- projet/character.py
+++ projet/character.py
@@ -5,22 +5,32 @@ class Character:
     def __init__(self, name, hp_max, attack_value, defense_value) -> None:
         self.name = name
         self.hp_max = hp_max
+        self.hp = hp_max
         self.attack_value = attack_value
         self.defense_value = defense_value
 
     def __str__(self) -> str:
-        return f"I'm {self.name} (att: {self.attack_value} / def: {self.defense_value})"
+        return f"""I'm {self.name}. Those are my caracs :
+    - {self.hp}/{self.hp_max} hp
+    - att: {self.attack_value}
+    - def: {self.defense_value}"""
     
+    def is_alive(self):
+        return self.hp > 0
 
-    # is_alive
-        # retourne un booléen
-    
-    # show_healthbar
-        # affiche une barre de vie sous la forme:
-        # [oooooo            ] 6/12hp
+    def decrease_hp(self, amount):
+        self.hp = self.hp - amount
 
-char1 = Character("James", 20, 8, 3)
+    def increase_hp(self, amount):
+        self.hp = self.hp + amount
+
+    def show_healthbar(self):
+        print(f"[{"o" * self.hp}{" " * (self.hp_max - self.hp)}] {self.hp}/{self.hp_max}hp")
+
+# char1 = Character("James", 20, 8, 3)
+# print(char1)
+
 char2 = Character("Lisa", 20, 8, 3)
-
-print(char1)
-print(char2)
+char2.decrease_hp(8)
+char2.decrease_hp(6)
+char2.show_healthbar()
