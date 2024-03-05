22653ee9f94d7ed5529ed42a744e32ab2719ed97
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-03-05 16:23:13 +0100

    update

--- projet/character.py
+++ projet/character.py
@@ -19,18 +19,32 @@ class Character:
         return self.hp > 0
 
     def decrease_hp(self, amount):
-        self.hp = self.hp - amount
+        self.hp -= amount
 
     def increase_hp(self, amount):
-        self.hp = self.hp + amount
+        self.hp += amount
 
     def show_healthbar(self):
         print(f"[{"o" * self.hp}{" " * (self.hp_max - self.hp)}] {self.hp}/{self.hp_max}hp")
+
+    def attack(self):
+        a_dice = Dice("red", 6)
+        damages = self.attack_value + a_dice.roll()
+        print(f"{self.name} attack with {damages} (att: {self.attack_value} + roll: {damages - self.attack_value})")
+        return damages
+    
+    def defense(self, damages):
+        a_dice = Dice("red", 6)
+        roll = a_dice.roll()
+        raw_damages = damages - self.defense_value - roll
+        print(f"{self.name} defend againt {damages} and took {raw_damages} damages ({damages} - def: {self.defense_value} - roll: {roll})")
+        return raw_damages
 
 # char1 = Character("James", 20, 8, 3)
 # print(char1)
 
 char2 = Character("Lisa", 20, 8, 3)
-char2.decrease_hp(8)
-char2.decrease_hp(6)
+dmg = char2.attack()
+dmg = char2.defense(dmg)
+char2.decrease_hp(dmg)
 char2.show_healthbar()
