7927bd5d484a658507472d5c2cd62b1c3560bbad
Gladrat <3368584+Gladrat@users.noreply.github.com>
2024-03-12 14:05:12 +0100

    update

---
+++ Groupes.md
@@ -0,0 +1,23 @@
+Groupe 01
+- Eva
+- Dylan
+- Baptiste
+- Mouhamadou
+
+Groupe 02
+- Ma2t
+- Rafou
+- Samuel
+- Ethan ?
+
+Groupe 03
+- Paolo
+- Shaïna
+- Col1
+- Sacha
+
+Groupe 04
+- Yannis
+- Adil
+- ???
+- ???
--- projet/character.py
+++ projet/character.py
@@ -4,6 +4,7 @@ from dice import Dice
     # S
     # D
 
+
 class Character:
     def __init__(self, name, hp_max, attack_value, defense_value, dice) -> None:
         self.name = name
@@ -18,43 +19,58 @@ class Character:
     - {self.hp}/{self.hp_max} hp
     - att: {self.attack_value}
     - def: {self.defense_value}"""
-    
+
     def is_alive(self):
         return self.hp > 0
 
     def decrease_hp(self, amount):
-        self.hp -= self.hp - amount
+        self.hp = self.hp - amount
         if self.hp < 0:
             self.hp = 0
+        self.show_healthbar()
 
     def increase_hp(self, amount):
         self.hp += amount
 
     def show_healthbar(self):
-        print(f"[{'o' * self.hp}{' ' * (self.hp_max - self.hp)}] {self.hp}/{self.hp_max}hp")
+        print(
+            f"[{'o' * self.hp}{' ' * (self.hp_max - self.hp)}] {self.hp}/{self.hp_max}hp"
+        )
 
-    def attack(self):
-        damages = self.attack_value + self.dice.roll()
-        print(f"{self.name} attack with {damages} (att: {self.attack_value} + roll: {damages - self.attack_value})")
-        return damages
-    
+    def compute_damages(self):
+        return self.attack_value + self.dice.roll()
+
+    def attack(self, target):
+        if self.is_alive():
+            damages = self.compute_damages()
+            print(
+                f"{self.name} attack with {damages} (att: {self.attack_value} + roll: {damages - self.attack_value})"
+            )
+            target.defense(damages)
+
     def defense(self, damages):
+        print(f"damages: {damages}")
         roll = self.dice.roll()
         raw_damages = damages - self.defense_value - roll
-        print(f"{self.name} defend againt {damages} and took {raw_damages} damages ({damages} - def: {self.defense_value} - roll: {roll})")
-        return raw_damages
+        print(
+            f"{self.name} defend againt {damages} and took {raw_damages} damages ({damages} - def: {self.defense_value} - roll: {roll})"
+        )
+        self.decrease_hp(raw_damages)
 
-char1 = Character("James", 20, 8, 3, Dice("red", 6))
-char2 = Character("Lisa", 20, 8, 3, Dice("blue", 6))
+
+class Warrior(Character):
+
+    def compute_damages(self):
+        return super().compute_damages() + 3
+    
+class Mage(Character):
+    # Bonus de +3 à la défense
+    pass
+
+
+char1 = Warrior("James", 20, 8, 3, Dice("red", 6))
+char2 = Warrior("Lisa", 20, 8, 3, Dice("blue", 6))
 
 while char1.is_alive() and char2.is_alive():
-    # ICI C'EST LE PIRE CODE POSSIBLE AU MONDE
-    dmg = char1.attack()
-    dmg = char2.defense(dmg)
-    char2.decrease_hp(dmg)
-    char2.show_healthbar()
-
-    dmg = char2.attack()
-    dmg = char1.defense(dmg)
-    char1.decrease_hp(dmg)
-    char1.show_healthbar()
+    char1.attack(char2)
+    char2.attack(char1)
